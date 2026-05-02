d45ce64868da052b4e61adaf0bac466e4f59c80b
GeovannaMenezess <cidamenazes@gmail.com>
2026-05-02 16:48:04 -0300

    rotas GET e POST de products funcionando

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
+import 'dotenv/config'
 import  express  from "express";
-const app = express()
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "./generated/prisma/client";
 
-app.get('/', function (req, res) {
-    res.send('servidor funcionando!')
+const app = express()
+app.use(express.json())
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+})
+
+
+// Testando o get
+// app.get('/', function (req, res) {
+//     res.send('servidor funcionando!')
+// })
+
+app.get('/products', async (req, res) => {
+    const product = await prisma.product.findMany()
+    res.json(product)
+})
+
+app.post( '/products', async (req, res) => {
+    const { name, description, price, stock_quantity} = req.body
+    
+    const product = await prisma.product.create({
+       data: {
+        name: name,
+       description : description,
+       price: price,
+       stock_quantity: stock_quantity
+       }
+    })
+    res.json(product)
 })
 
 app.listen(3000, () => {
