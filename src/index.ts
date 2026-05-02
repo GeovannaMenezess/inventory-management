dda9776610d830d9a799a060b0a3251021888029
GeovannaMenezess <cidamenazes@gmail.com>
2026-05-01 23:53:03 -0300

    criação do servidor express com primeira rota

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('typescript funcionando')
+import  express  from "express";
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('servidor funcionando!')
+})
+
+app.listen(3000, () => {
+    console.log('servidor rodando em http://localhost:3000')
+})
